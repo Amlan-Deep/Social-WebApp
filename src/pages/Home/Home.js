8c219e33e5b2c4bc263c579d481b76ab3e82b2e7
Amlan Deep Mohapatra <amlandeep09012003@gmail.com>
2023-06-09 11:11:25 +0530

    commit 9th june

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import './Home.css';
 import ProfileSide from '../../components/ProfileSide/ProfileSide';
+import PostSide from '../../components/PostSide/PostSide';
+import RightSide from '../../components/RightSide/RightSide';
 
 const Home = () => {
   return (
     <div className='Home'>
         <ProfileSide/>
-        <div className='postSide'>post</div>
-        <div className='rightSide'>right</div>
+        <PostSide/>
+        <RightSide/>
     </div>
   )
 }
